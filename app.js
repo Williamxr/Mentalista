de27cb80bcf3a34343486ebb4fc30b43156d10c8
William <william.teletabs@hotmail.com>
2022-03-17 18:37:10 -0300

    Criando resultado.innerHtml para mostrar uma mensagem de texto informando se o usuário acertou ou errou o número sorteado

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var numeroSecreto = parseInt(Math.random() * 11);
-
 function Chutar() {
+  var numeroSecreto = parseInt(Math.random() * 11);
+
+  var resultado = document.getElementById("resultado");
+
   var chute = parseInt(document.getElementById("valor").value);
 
   if (chute == numeroSecreto) {
-    console.log("alo");
+    resultado.innerHTML = "Parabéns você acertou";
   } else if (chute >= 11 || chute < 0) {
-    console.log("ta sabendo legal");
+    resultado.innerHTML = "Você deve digitar um número de 0 a 10";
   } else {
-    console.log("erro burrao");
+    resultado.innerHTML = "Você errou, o número secreto era " + numeroSecreto;
   }
 }
