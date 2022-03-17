e9c838a0d91d5577d4567b4f418b0f9fceabede9
William <william.teletabs@hotmail.com>
2022-03-17 18:10:59 -0300

    Criando condição para número sorteado e adicionando parseInt para o número sorteado

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var numeroSecreto = 3;
+var numeroSecreto = parseInt(Math.random() * 11);
 
 function Chutar() {
   var chute = parseInt(document.getElementById("valor").value);
-  console.log(chute);
+
+  if (chute == numeroSecreto) {
+    console.log("alo");
+  } else if (chute >= 11 || chute < 0) {
+    console.log("ta sabendo legal");
+  } else {
+    console.log("erro burrao");
+  }
 }
